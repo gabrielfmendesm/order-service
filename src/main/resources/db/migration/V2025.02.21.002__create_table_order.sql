fff9e442e4c1bf8a6b79692166c1b095d0a41929
gabrielfmendesm <gabrielfmm@al.insper.edu.br>
2025-05-24 09:03:26 -0300

    fix: order service

--- src/main/resources/db/migration/V2025.02.21.002__create_table_order.sql
+++ src/main/resources/db/migration/V2025.02.21.002__create_table_order.sql
@@ -1,9 +1,7 @@
-    CREATE TABLE orders (
-        id_order      VARCHAR(36)    NOT NULL,
-        id_account    VARCHAR(36)    NOT NULL,
-        dt_date       TIMESTAMP      NOT NULL,
-        nu_total      DECIMAL(10,2)  NOT NULL,
-        CONSTRAINT pk_order PRIMARY KEY (id_order),
-        CONSTRAINT fk_order_account FOREIGN KEY (id_account)
-            REFERENCES account(id_account)
-    );
+CREATE TABLE orders (
+  id_order      VARCHAR(36)    NOT NULL,
+  id_account    VARCHAR(36)    NOT NULL,
+  dt_date       TIMESTAMP      NOT NULL,
+  nu_total      DECIMAL(10,2)  NOT NULL,
+  CONSTRAINT pk_order PRIMARY KEY (id_order)
+);
--- src/main/resources/db/migration/V2025.02.21.003__create_table_orders_item.sql
+++ src/main/resources/db/migration/V2025.02.21.003__create_table_orders_item.sql
@@ -5,8 +5,4 @@ CREATE TABLE orders_item (
     nu_quantity   INTEGER        NOT NULL,
     nu_total      DECIMAL(10,2)  NOT NULL,
     CONSTRAINT pk_order_item PRIMARY KEY (id_item),
-    CONSTRAINT fk_item_order FOREIGN KEY (id_order)
-        REFERENCES "order"(id_order),
-    CONSTRAINT fk_item_product FOREIGN KEY (id_product)
-        REFERENCES product(id_product)
 );
